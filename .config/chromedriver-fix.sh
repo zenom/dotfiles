83f19ea2a178a34ec9cb2d7f6aa53f302de02849
Andy Holman <andy@conspyre.com>
2023-08-21 15:58:31 -0400

    Update kitty and brew.

--- .config/chromedriver-fix.sh
+++ .config/chromedriver-fix.sh
@@ -1,2 +1,4 @@
 #!/bin/zsh
-xattr -d com.apple.quarantine ~/.local/bin/chromedriver
+# xattr -d com.apple.quarantine "/Users/andy/.local/bin/chromedriver"
+xattr -d com.apple.quarantine $(brew --prefix)/bin/chromedriver
+
