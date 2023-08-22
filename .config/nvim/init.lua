d7364cfd45ce6969259d1b5929fb4f3ce8460dfc
Andy Holman <andy@conspyre.com>
2023-08-22 15:29:47 -0400

    Update telescope finders.

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -231,20 +231,6 @@ require('telescope').setup {
       },
     },
   },
-  pickers = {
-    find_files = {
-      theme = 'ivy'
-    },
-    grep_string = {
-      theme = 'ivy'
-    },
-    git_files = {
-      theme = 'dropdown'
-    },
-    live_grep = {
-      theme = 'ivy'
-    }
-  }
 }
 
 -- Enable telescope fzf native, if installed
@@ -252,21 +238,36 @@ pcall(require('telescope').load_extension, 'fzf')
 
 -- See `:help telescope.builtin`
 vim.keymap.set('n', '<leader>?', require('telescope.builtin').oldfiles, { desc = '[?] Find recently opened files' })
-vim.keymap.set('n', '<leader>sb', require('telescope.builtin').buffers, { desc = '[ ] Find existing buffers' })
+vim.keymap.set('n', '<leader>sb', function()
+  require('telescope.builtin').buffers(require('telescope.themes').get_dropdown {
+    previewer = false,
+  })
+end, { desc = '[ ] Find existing buffers' })
+
 vim.keymap.set('n', '<leader>/', function()
   -- You can pass additional configuration to telescope to change theme, layout, etc.
   require('telescope.builtin').current_buffer_fuzzy_find(require('telescope.themes').get_dropdown {
-    winblend = 10,
     previewer = false,
   })
 end, { desc = '[/] Fuzzily search in current buffer' })
 
-vim.keymap.set('n', '<leader>gf', require('telescope.builtin').git_files, { desc = 'Search [G]it [F]iles' })
-vim.keymap.set('n', '<leader>sf', require('telescope.builtin').find_files, { desc = '[S]earch [F]iles' })
+vim.keymap.set('n', '<leader>sf', function()
+  require('telescope.builtin').find_files(require('telescope.themes').get_dropdown { previewer = false })
+end, { desc = '[S]earch [F]iles' })
 vim.keymap.set('n', '<leader>sh', require('telescope.builtin').help_tags, { desc = '[S]earch [H]elp' })
 vim.keymap.set('n', '<leader>sw', require('telescope.builtin').grep_string, { desc = '[S]earch current [W]ord' })
-vim.keymap.set('n', '<leader>sg', require('telescope.builtin').live_grep, { desc = '[S]earch by [G]rep' })
-vim.keymap.set('n', '<leader>sd', require('telescope.builtin').diagnostics, { desc = '[S]earch [D]iagnostics' })
+
+vim.keymap.set('n', '<leader>sg', function()
+  require('telescope.builtin').live_grep(require('telescope.themes').get_dropdown { previewer = false })
+end, { desc = '[S]earch by [G]rep' })
+
+vim.keymap.set('n', '<leader>sd', function()
+  require('telescope.builtin').diagnostics(require('telescope.themes').get_dropdown { previewer = false })
+end, { desc = '[S]earch [D]iagnostics' })
+
+vim.keymap.set('n', '<leader>sq', function()
+  require('telescope.builtin').quickfix(require('telescope.themes').get_dropdown { previewer = false })
+end, { desc = '[S]earch [Q]uickfix' })
 
 -- [[ Configure Treesitter ]]
 -- See `:help nvim-treesitter`
