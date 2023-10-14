1ad4cc352432f06e26d132a33fb15c4e722d9acb
Andrew Holman <andy@Andrews-Mac-mini-2.local>
2023-10-14 19:46:28 -0400

    Add lualine and update theme.

--- .config/nvim/lua/zenom/plugins/theme.lua
+++ .config/nvim/lua/zenom/plugins/theme.lua
@@ -3,20 +3,36 @@ return {
   name = "catppuccin",
   priority = 1000,
   config = function()
-    vim.api.nvim_set_hl(0, 'Normal', { bg = 'none' })
-    vim.api.nvim_set_hl(0, 'NormalFloat', { bg = 'none' })
     require('catppuccin').setup({
       transparent_background = true,
       term_colors = true,
       integrations = {
-        gitsigns = true,
-        neogit = true,
-        nvimtree = true,
-        telescope = {
-          enabled = true,
-        },
-        treesitter = true,
-        -- which_key = true,
+	cmp = true,
+	gitsigns = true,
+	nvimtree = true,
+	telescope = {
+	  enabled = true,
+	},
+	treesitter = true,
+	dashboard = true,
+	native_lsp = {
+	  enabled = true,
+	  virtual_text = {
+	    errors = { "italic" },
+	    hints = { "italic" },
+	    warnings = { "italic" },
+	    information = { "italic" },
+	  },
+	  underlines = {
+	    errors = { "underline" },
+	    hints = { "underline" },
+	    warnings = { "underline" },
+	    information = { "underline" },
+	  },
+	  inlay_hints = {
+	    background = true,
+	  },
+	},
       }
     })
     vim.cmd.colorscheme 'catppuccin'
