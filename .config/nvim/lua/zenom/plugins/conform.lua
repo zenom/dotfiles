bd786e1abce9992a66300f1ffce105b043f2c6d0
Andrew Holman <andy@Andrews-Mac-mini-2.local>
2023-10-15 09:20:38 -0400

    More changes.

--- .config/nvim/lua/zenom/plugins/conform.lua
+++ .config/nvim/lua/zenom/plugins/conform.lua
@@ -2,16 +2,13 @@ return {
 	'stevearc/conform.nvim',
 	lazy = true,
 	event = { 'BufReadPre', 'BufNewFile' },
-	-- keys = {
-	--   { '<leader>f', function() require('conform').format() end }
-	-- },
 	config = function()
 		local conform = require('conform')
 
 		conform.setup({
 			formatters_by_ft = {
 				ruby = { 'rufo', 'rubocop' }, --rubocop, standardrb, rufo
-				eruby = { 'erb_format', 'rubocop', 'htmlbeautifier' },
+				eruby = { 'erb_format', 'rubocop' },
 				lua = { 'stylua' },
 				javascript = { 'prettier' },
 				css = { 'prettier' },
@@ -33,6 +30,6 @@ return {
 				async = false,
 				timeout_ms = 1000,
 			})
-		end, { desc = 'Format file or range (in visual mode)' })
+		end, {})
 	end,
 }
