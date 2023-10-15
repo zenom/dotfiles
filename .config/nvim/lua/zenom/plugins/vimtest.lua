0e7dee21439a70fd34b215c5e0177ff85ee680a5
Andy Holman <andy@conspyre.com>
2023-10-15 16:14:50 -0400

    Use vim-test.

--- .config/nvim/lua/zenom/plugins/vimtest.lua
+++ .config/nvim/lua/zenom/plugins/vimtest.lua
@@ -1,9 +1,14 @@
 return {
 	'vim-test/vim-test',
+	dependencies = {
+		'preservim/vimux',
+		'christoomey/vim-tmux-navigator',
+	},
 	config = function()
-		vim.keymap.set('n', '<leader>tf', '<cmd>:TestFile<CR>', { desc = '[T]est [F]ile' })
-		vim.keymap.set('n', '<leader>tn', '<cmd>:TestNearest<CR>', { desc = '[T]est [N]earest' })
-		vim.g['test#strategy'] = 'neovim'
+		vim.keymap.set('n', '<leader>tf', '<cmd>:TestFile<CR>', {})
+		vim.keymap.set('n', '<leader>tn', '<cmd>:TestNearest<CR>', {})
+		vim.keymap.set('n', '<leader>tt', '<cmd>:VimuxTogglePane<CR>', {})
+		vim.g['test#strategy'] = 'vimux'
 		vim.g['test#echo_command'] = 0
 		vim.g['test#neovim#term_position'] = 'botright 20'
 	end,
