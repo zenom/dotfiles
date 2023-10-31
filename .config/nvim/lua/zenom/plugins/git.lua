6ea0abb5d3a260882615cd0f57a044a062970384
Andy Holman <andy@conspyre.com>
2023-10-31 09:00:27 -0400

    Update to neogit.

--- .config/nvim/lua/zenom/plugins/git.lua
+++ .config/nvim/lua/zenom/plugins/git.lua
@@ -1,6 +1,24 @@
+-- return {
+-- 	'tpope/vim-fugitive',
+-- 	config = function()
+-- 		vim.keymap.set('n', '<leader>gg', vim.cmd.Git)
+-- 	end,
+-- }
+
 return {
-	'tpope/vim-fugitive',
+	'NeogitOrg/neogit',
+	dependencies = {
+		'nvim-lua/plenary.nvim', -- required
+		'nvim-telescope/telescope.nvim', -- optional
+		'sindrets/diffview.nvim', -- optional
+		'ibhagwan/fzf-lua', -- optional
+	},
 	config = function()
-		vim.keymap.set('n', '<leader>gg', vim.cmd.Git)
+		local neogit = require('neogit')
+		neogit.setup({})
+
+		vim.keymap.set('n', '<leader>gg', function()
+			neogit.open({ kind = 'split_above' })
+		end)
 	end,
 }
