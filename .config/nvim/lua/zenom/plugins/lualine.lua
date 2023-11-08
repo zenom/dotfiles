b3f9687f365d87b159179426f81f837d2d626ebe
Andy Holman <andy@conspyre.com>
2023-11-08 10:21:23 -0500

    Latest changes.

--- .config/nvim/lua/zenom/plugins/lualine.lua
+++ .config/nvim/lua/zenom/plugins/lualine.lua
@@ -5,22 +5,22 @@ return {
 	},
 	config = function()
 		local lualine = require('lualine')
-		local lazy_status = require('lazy.status')
+		-- local lazy_status = require('lazy.status')
 
 		lualine.setup({
 			options = { theme = 'tokyonight' },
-			sections = {
-				lualine_x = {
-					{
-						lazy_status.updates,
-						cond = lazy_status.has_updates,
-						color = { fg = '#ff9e64' },
-					},
-					{ 'encoding' },
-					{ 'fileformat' },
-					{ 'filetype' },
-				},
-			},
+			-- sections = {
+			-- 	-- lualine_x = {
+			-- 	-- 	{
+			-- 	-- 		lazy_status.updates,
+			-- 	-- 		cond = lazy_status.has_updates,
+			-- 	-- 		color = { fg = '#ff9e64' },
+			-- 	-- 	},
+			-- 	-- 	{ 'encoding' },
+			-- 	-- 	{ 'fileformat' },
+			-- 	-- 	{ 'filetype' },
+			-- 	-- },
+			-- },
 		})
 	end,
 }
